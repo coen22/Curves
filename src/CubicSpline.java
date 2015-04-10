5b61ab9c7a274cffbff4d59d80b9c9a3149ee9ef
Coen Hacking <coen.hacking@gmail.com>
2015-04-10 11:13:59 +0200

    some setup

---
+++ src/CubicSpline.java
@@ -0,0 +1,20 @@
+import java.awt.geom.Point2D;
+
+
+public class CubicSpline implements iCurve {
+
+	public Point2D[] plot() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public double length() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public double area() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
---
+++ src/iCurve.java
@@ -0,0 +1,7 @@
+import java.awt.geom.Point2D;
+
+public interface iCurve {
+	public Point2D[] plot();
+	public double length();
+	public double area();
+}
