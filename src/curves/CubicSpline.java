353457669b4cbe338bdd6d8ceb4dd0f620574550
davidr88 <david.roschewitz@gmail.com>
2015-04-17 22:59:55 +0200

    edits

--- src/curves/CubicSpline.java
+++ src/curves/CubicSpline.java
@@ -6,13 +6,20 @@ import java.util.List;
 
 
 public class CubicSpline extends Curve {
+	public static final int NATURAL_SPLINE = 1;
+	public static final int CLAMPED_SPLINE = 2;
+	public static final int NOT_A_KNOT_SPLINE = 3;
+	
+	private static final double EPSILON = 1e-10;
 	
 	private ArrayList<double[]> Xcoefficients;
 	private ArrayList<double[]> Ycoefficients; 
+	private int type;
 
-	public CubicSpline(Point2D point, String name){
+	public CubicSpline(Point2D point, String name, int type){
 		super(name);
-		super.add(point.getX(), point.getY());
+		add(point.getX(), point.getY());
+		this.type = type;
 	}
 	
 	public double length() {
@@ -28,4 +35,73 @@ public class CubicSpline extends Curve {
 	public List<Point2D> calcPlotingCoordinates(double tInterval) {
 		return null;
 	}
+	
+	public void add(double x, double y){
+		super.points.add(new Point2D.Double(x,y));
+	}
+	
+	//currently works for natural spline
+	private void calcCoefficients(){
+		Double[][] CMatrix = new Double[super.points.size()][super.points.size()];
+		Double[] XvectorC = new Double[super.points.size()];
+		
+		if (type == NATURAL_SPLINE){
+			for (int i = 1; i < XvectorC.length-1; i++){
+				XvectorC[i] = 3 * (super.points.get(i-1).getX() - (2*super.points.get(i).getX()) + super.points.get(i+1).getX());
+			}
+			for (int i = 1; i < CMatrix.length-1; i++){
+				CMatrix[i][i] = (double) 1;
+				CMatrix[i][i+1] = (double) 4;
+				CMatrix[i][i+2] = (double) 1;
+			}
+		}
+		
+		
+	}
+	
+	
+	//Gaussian elimination with partial pivoting
+	//method for temporary testing copied from: http://introcs.cs.princeton.edu/java/95linear/GaussianElimination.java.html
+	//all credit belongs to original authors: Robert Sedgewick and Kevin Wayne
+    public static double[] lsolve(double[][] A, double[] b) {
+        int N  = b.length;
+
+        for (int p = 0; p < N; p++) {
+
+            // find pivot row and swap
+            int max = p;
+            for (int i = p + 1; i < N; i++) {
+                if (Math.abs(A[i][p]) > Math.abs(A[max][p])) {
+                    max = i;
+                }
+            }
+            double[] temp = A[p]; A[p] = A[max]; A[max] = temp;
+            double   t    = b[p]; b[p] = b[max]; b[max] = t;
+
+            // singular or nearly singular
+            if (Math.abs(A[p][p]) <= EPSILON) {
+                throw new RuntimeException("Matrix is singular or nearly singular");
+            }
+
+            // pivot within A and b
+            for (int i = p + 1; i < N; i++) {
+                double alpha = A[i][p] / A[p][p];
+                b[i] -= alpha * b[p];
+                for (int j = p; j < N; j++) {
+                    A[i][j] -= alpha * A[p][j];
+                }
+            }
+        }
+
+        // back substitution
+        double[] x = new double[N];
+        for (int i = N - 1; i >= 0; i--) {
+            double sum = 0.0;
+            for (int j = i + 1; j < N; j++) {
+                sum += A[i][j] * x[j];
+            }
+            x[i] = (b[i] - sum) / A[i][i];
+        }
+        return x;
+    }
 }
