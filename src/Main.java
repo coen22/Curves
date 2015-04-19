a481f06cb2fdc72623c383890b9281cf99edb326
Coen Hacking <coen.hacking@gmail.com>
2015-04-19 18:08:08 +0200

    added main method

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,15 @@
+import javax.swing.JFrame;
+
+import curves.Canvas;
+
 
 public class Main {
-
 	public static void main(String[] args) {
-
+		JFrame f = new JFrame();
+		Canvas c = new Canvas(1, 100);
+		
+		f.setContentPane(c);
+		f.setSize(800, 600);
+		f.setVisible(true);
 	}
-
 }
