7831aa095d382911a2b07eff4673395d1dfc0cfa
davidr88 <david.roschewitz@gmail.com>
2015-05-26 11:08:11 +0200

    ellipse fix

--- src/curves/Ellipse.java
+++ src/curves/Ellipse.java
@@ -106,13 +106,14 @@ public class Ellipse extends Curve{
 	}
 	@Override
 	protected ArrayList<Point2D> getPlot(int subPoints) {
-		subPoints *= 10;
-		if(subPoints < 1000){
-			subPoints = 1000;
+		if (currentSubPoints == subPoints){
+			return plot;
 		}
-		calcPlot(subPoints);
-		System.out.println("Subpoints: " + subPoints);
-		return plot;
+		else {
+			currentSubPoints = subPoints;
+			calcPlot(subPoints);
+			return plot;
+		}
 	}
 	
 	@Override
